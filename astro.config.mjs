7d796ac7aa8441aef35fbf931c5ceb652cea6557
Christopher Tay <tay.christopher@gmail.com>
2025-08-24 17:13:05 +0800

    Add new blog post on mathematical notation, update homepage, create tags and TIL pages, and configure Tailwind CSS. Still problems with math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://mengkeat.github.io',
-  output: 'static'
+  output: 'static',
+  integrations: [
+    tailwind(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    })
+  ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
 });
